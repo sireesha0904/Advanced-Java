db91c8d71f6aa90ef844663d489b0e353e934ad6
ModiSireesha <modisireesha09@gmail.com>
2024-10-18 22:23:15 +0530

    Multi-Threading

---
+++ Multithread.java
@@ -0,0 +1,10 @@
+public class Multithread {
+    public static void main(String[] args) {
+        Multithread t = new Multithread();
+        t.run();
+    }
+    
+    public void run() {
+        System.out.println("Thread is running");
+    }
+}
