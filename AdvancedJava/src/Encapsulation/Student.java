ea3f228deaa25a53f03578039f33d594be2610ed
ModiSireesha <modisireesha09@gmail.com>
2024-10-21 21:00:02 +0530

    created Encapsulation

---
+++ AdvancedJava/src/Encapsulation/Student.java
@@ -0,0 +1,7 @@
+//It refers to the bundling of data (fields or variables) and the methods (functions) that operate on the data into a single unit, or class. 
+
+package Encapsulation;
+
+public class Student {
+
+}
