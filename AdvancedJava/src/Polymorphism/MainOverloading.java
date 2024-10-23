fa2ca668e7f4c3197d51fada7188856bbf2ff649
ModiSireesha <modisireesha09@gmail.com>
2024-10-23 19:04:18 +0530

    updated

--- AdvancedJava/src/Polymorphism/MainOverloading.java
+++ AdvancedJava/src/Polymorphism/MainOverloading.java
@@ -1,3 +1,4 @@
+//Method overloading occurs when two or more methods in the same class have the same method name but different parameters
 package Polymorphism;
 
 public class MainOverloading {
---
+++ AdvancedJava/src/Polymorphism/MainOverriding.java
@@ -0,0 +1,6 @@
+//Method overriding occurs when a subclass provides a specific implementation for a method that is already defined in its parent class. 
+package Polymorphism;
+
+public class MainOverriding {
+
+}
