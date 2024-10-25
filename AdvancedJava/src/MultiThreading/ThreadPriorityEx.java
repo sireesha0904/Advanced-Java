afc1501be2a53cb93edd13c387ec13e038400064
ModiSireesha <modisireesha09@gmail.com>
2024-10-25 21:44:00 +0530

    updated

--- AdvancedJava/src/MultiThreading/ThreadPriorityEx.java
+++ AdvancedJava/src/MultiThreading/ThreadPriorityEx.java
@@ -5,4 +5,14 @@ public class ThreadPriorityEx extends Thread{
 	public void run() {
 		System.out.println(Thread.currentThread().getName() + "is running with priority " + Thread.currentThread().getPriority());
 	}
+	
+	public static void main(String args[]) {
+		ThreadPriorityEx t1 = new ThreadPriorityEx();
+		ThreadPriorityEx t2 = new ThreadPriorityEx();
+		ThreadPriorityEx t3 = new ThreadPriorityEx();
+		
+		t1.setPriority(MIN_PRIORITY);
+		t2.setPriority(MIN_PRIORITY);
+		t3.setPriority(7);
+	}
 }
