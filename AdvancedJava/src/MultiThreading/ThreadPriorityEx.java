280d161a4b87e27f0ea86d32d58cabe40a2a1bd1
ModiSireesha <modisireesha09@gmail.com>
2024-10-25 21:42:05 +0530

    updated

--- AdvancedJava/src/MultiThreading/ThreadPriorityEx.java
+++ AdvancedJava/src/MultiThreading/ThreadPriorityEx.java
@@ -2,4 +2,7 @@ package MultiThreading;
 
 public class ThreadPriorityEx extends Thread{
 
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + "is running with priority " + Thread.currentThread().getPriority());
+	}
 }
