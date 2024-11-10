27a5f257d382be595736ba8e68c181c3fd3cf8b7
sireesha0904 <modisireesha09@gmail.com>
2024-11-10 09:32:26 +0530

    updated threads

--- AdvancedJava/src/MultiThreading/CustomThread.java
+++ AdvancedJava/src/MultiThreading/CustomThread.java
@@ -1,13 +1,17 @@
+//Creating Thread by Extending Thread Class
 package MultiThreading;
 
-public class CustomThread {
+public class CustomThread extends Thread{
 
-	public void run() {
-		System.out.println("Thread is running");
+	public void run() { // used to perform action of the thread
+		System.out.println("code running in the Thread");
 	}
 	
 	public static void main(String[] args) {
-		CustomThread t = new CustomThread();
-		t.run();
+		CustomThread th = new CustomThread();
+		th.start();  // Execution of the thread and calls the run() method
+		System.out.println("Code is running outside the Thread");
+		
 	}
+
 }
