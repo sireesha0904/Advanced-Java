310cddd202d7a04d76122f6fe7dec8811f0d5414
sireesha0904 <modisireesha09@gmail.com>
2024-11-10 20:37:14 +0530

    updated

--- AdvancedJava/src/MultiThreading/Multi.java
+++ AdvancedJava/src/MultiThreading/Multi.java
@@ -1,11 +1,10 @@
 //Java Thread Example by implementing Runnable interface
-
 package MultiThreading;
 
 public class Multi implements Runnable{
 
-	public void run() {
-		System.out.println("Thread is Running...");
+	public void run() { // perform action of a Thread
+		System.out.println("Thread is Running..."); 
 	}
 	
 	public static void main(String[] args) {
