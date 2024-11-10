c0de245a4f1cbe6d4a5d74ce5e54bf86a7897ce7
sireesha0904 <modisireesha09@gmail.com>
2024-11-10 10:44:43 +0530

    Daemon Thread example

--- AdvancedJava/src/MultiThreading/TestDaemonThread.java
+++ AdvancedJava/src/MultiThreading/TestDaemonThread.java
@@ -1,5 +1,18 @@
 package MultiThreading;
 
-public class TestDaemonThread {
+public class TestDaemonThread extends Thread{
 
+	public void run() {
+		System.out.println("Daemon-Thread Test : " + Thread.currentThread().getName() + " " + Thread.currentThread().isDaemon());
+	}
+	public static void main(String[] args) {
+		TestDaemonThread th1 = new TestDaemonThread();
+		TestDaemonThread th2 = new TestDaemonThread();
+		
+		th1.setDaemon(true);
+		th2.setDaemon(false);
+		
+		th1.start();
+		th2.start();
+	}
 }
