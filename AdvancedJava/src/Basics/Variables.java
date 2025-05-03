0aa681f4f1993327dd34798387108e96003897b3
sireesha0904 <modisireesha09@gmail.com>
2025-05-03 21:42:49 +0530

    basics

---
+++ AdvancedJava/src/Basics/Variables.java
@@ -0,0 +1,13 @@
+package Basics;
+// 2 steps to create a Variables
+// 1. Declaration
+// 2. assignment
+
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int age = 20;
+		System.out.print(age);
+	}
+}
