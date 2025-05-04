b9f531cff33a1bc4fa526929bf4a31466071b0a1
sireesha0904 <modisireesha09@gmail.com>
2025-05-04 13:29:36 +0530

    basic i/o

--- AdvancedJava/src/Basics/UserIO.java
+++ AdvancedJava/src/Basics/UserIO.java
@@ -15,9 +15,13 @@ public class UserIO {
 		System.out.print("Enter your grade here : ");
 		double grade = sc.nextDouble();
 		
+		System.out.print("Are you a student? (true/false)");
+		boolean student = sc.nextBoolean();
 		
 		System.out.println("Hello "+ name );
 		System.out.println("You are "+age + " Years Old.");
 		System.out.println("Your grade is "+ grade);
+		System.out.println("Student : "+student);
+		
 	}
 }
