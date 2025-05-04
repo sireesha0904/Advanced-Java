ef2c9313ceec1f713ae801b8a748a2fb68da6efc
sireesha0904 <modisireesha09@gmail.com>
2025-05-04 14:40:44 +0530

    mad libs game

--- AdvancedJava/src/Basics/MadLibsGame.java
+++ AdvancedJava/src/Basics/MadLibsGame.java
@@ -5,5 +5,27 @@ public class MadLibsGame {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		
+		String adj;
+		String animal;
+		String place;
+		String verb;
+		String noun;
+		
+		
+		System.out.print("Adjective ");
+		adj = sc.nextLine();
+		System.out.print("Animal ");
+		animal = sc.nextLine();
+		System.out.print("Place ");
+		place = sc.nextLine();
+		System.out.print("Verb ");
+		verb = sc.nextLine();
+		System.out.print("Noun ");
+		noun = sc.nextLine();
+		
+		System.out.println("Today I saw a " + adj + " " + animal + " at the " + place + ".");
+		System.out.println("It was trying to " + verb + " a " + noun + "!");
+
 	}
 }
