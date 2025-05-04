5813f7023f5b697a68d3b41f844c7f5d516e661b
sireesha0904 <modisireesha09@gmail.com>
2025-05-04 15:29:53 +0530

    basics

---
+++ AdvancedJava/src/Basics/ArithmeticOpre.java
@@ -0,0 +1,29 @@
+package Basics;
+
+public class ArithmeticOpre {
+
+	public static void main(String[] args) {
+		//Arithmetic Operations
+		
+		int a = 10;
+		int b = 5;
+		
+		int sum = a+b;
+		int sub = a-b;
+		int mult = a*b;
+		int div = a/b;
+		int modu = a%b;
+		
+		System.out.println(sum);
+		System.out.println(sub);
+		System.out.println(mult);
+		System.out.println(div);
+		System.out.println(modu);
+		
+		//Assignment operators
+		 a = a+b; //(a+=b)
+		 a -= b;
+		 System.out.println(a);
+		 System.out.println(b);
+	}
+}
