f042993ab83838e28b7877ad5fb86d2cb6387c66
sireesha0904 <modisireesha09@gmail.com>
2025-05-05 07:58:27 +0530

    Math Classes

--- AdvancedJava/src/Basics/MathClasses.java
+++ AdvancedJava/src/Basics/MathClasses.java
@@ -2,4 +2,11 @@ package Basics;
 
 public class MathClasses {
 
+	public static void main(String[] args) {
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.E); 
+		System.out.println(Math.pow(2, 3));
+		
+	}
 }
