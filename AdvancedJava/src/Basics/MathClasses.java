d280669f74ded53e787d0754d331a959ae4d7bf0
sireesha0904 <modisireesha09@gmail.com>
2025-05-05 08:00:33 +0530

    Math classes

--- AdvancedJava/src/Basics/MathClasses.java
+++ AdvancedJava/src/Basics/MathClasses.java
@@ -7,6 +7,8 @@ public class MathClasses {
 		System.out.println(Math.PI);
 		System.out.println(Math.E); 
 		System.out.println(Math.pow(2, 3));
+		System.out.println(Math.abs(-6));
+		System.out.println(Math.sqrt(16));
 		
 	}
 }
