7d37d165e2c6d808fd89294a7af4dcc450b5fb99
sireesha0904 <modisireesha09@gmail.com>
2025-05-05 09:01:14 +0530

    Printf Statements

--- AdvancedJava/src/Basics/MathClasses.java
+++ AdvancedJava/src/Basics/MathClasses.java
@@ -11,5 +11,14 @@ public class MathClasses {
 		System.out.println(Math.sqrt(16));
 		System.out.println(Math.round(16.25));
 		
+		
+		//In Java, printf is used to print formatted output to the console
+		String name = "Sireesha";
+		int age = 20; 
+		double score = 8.7;
+		
+		System.out.printf("Name %s\n", name);
+		System.out.printf("Age is %d\n", age);
+		System.out.printf("CGPA is %f", score);
 	}
 }
