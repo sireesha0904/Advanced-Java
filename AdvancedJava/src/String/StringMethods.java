6da9a0b62fd3ce739ce02bb82d9349639a8e60d6
sireesha0904 <modisireesha09@gmail.com>
2025-05-06 21:51:55 +0530

    String methods

--- AdvancedJava/src/String/StringMethods.java
+++ AdvancedJava/src/String/StringMethods.java
@@ -2,4 +2,17 @@ package String;
 
 public class StringMethods {
 
+	public static void main(String[] args) {
+		
+		String name = "Sireesha";
+		
+		int length = name.length();
+		char letter = name.charAt(6);
+		int indx = name.indexOf("e");
+		
+		System.out.println("Length is : " + length);
+		System.out.println("Letter at index 6th : " + letter);
+		System.out.println("Index of letter e is : "+indx);
+		
+	}
 }
