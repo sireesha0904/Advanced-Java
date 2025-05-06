5d89fc37cf9b8219836dec1d7047acb3e3d37aed
sireesha0904 <modisireesha09@gmail.com>
2025-05-06 22:16:59 +0530

    Sub Strings

--- AdvancedJava/src/String/SubStrings.java
+++ AdvancedJava/src/String/SubStrings.java
@@ -4,5 +4,14 @@ public class SubStrings {
 
 	public static void main(String[] args) {
 		
+		String email = "modisireesha09@gmail.com";
+		
+		String username = email.substring(0,12);
+		String host = email.substring(15);
+		
+		
+		System.out.println("User name : "+ username);
+		System.out.println("Host name : "+ host);
+		
 	}
 }
