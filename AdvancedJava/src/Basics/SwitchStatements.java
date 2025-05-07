cd9e1e3a03724530dc7cd78c071c82faa1be138d
sireesha0904 <modisireesha09@gmail.com>
2025-05-07 20:12:28 +0530

    Switch Statements

--- AdvancedJava/src/Basics/SwitchStatements.java
+++ AdvancedJava/src/Basics/SwitchStatements.java
@@ -13,18 +13,27 @@ public class SwitchStatements {
 		switch(n) {
 		case 1 :
 			System.out.println("Monday");
+			break;
 		case 2:
 			System.out.println("Tuesday");
+			break;
 		case 3:
 			System.out.println("Wednesday");
+			break;
 		case 4:
 			System.out.println("Thursday");
+			break;
 		case 5:
 			System.out.println("Friday");
+			break;
 		case 6:
 			System.out.println("Saurday");
+			break;
 		case 7: 
 			System.out.println("Sunday");
+			break;
+		default: 
+			System.out.println("Enter the valid Number");
 		}
 	}
 }
