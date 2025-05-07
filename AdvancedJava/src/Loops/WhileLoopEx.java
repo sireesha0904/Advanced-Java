55c06141242de4785310ca43b6c7aacbf9c97ef3
sireesha0904 <modisireesha09@gmail.com>
2025-05-07 20:22:42 +0530

    While Loops

---
+++ AdvancedJava/src/Loops/WhileLoopEx.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class WhileLoopEx {
+
+	public static void main(String[] args) {
+		
+		int n = 1;
+		while (n<=6) {
+			System.out.println("Hello!!");
+			n++;
+		}
+	}
+}
