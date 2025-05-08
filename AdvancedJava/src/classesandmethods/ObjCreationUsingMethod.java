9a8670d517039c48c777f6fcde2f20c6cdc8fd7c
sireesha0904 <modisireesha09@gmail.com>
2025-05-08 21:54:10 +0530

    Creating objects using Methods

---
+++ AdvancedJava/src/classesandmethods/ObjCreationUsingMethod.java
@@ -0,0 +1,29 @@
+package classesandmethods;
+
+class Students{
+	int id;
+	String name;
+	String dept;
+	int age;
+	
+	
+	void insertData(int i, String n, String d, int a) {
+		id = i;
+		name = n;
+		dept = d;
+		age = a;
+		
+		}
+	
+	void display() {
+		System.out.println("Hello ");
+	}
+	}
+
+
+public class ObjCreationUsingMethod {
+
+	public static void main(String[] args) {
+		
+	}
+}
