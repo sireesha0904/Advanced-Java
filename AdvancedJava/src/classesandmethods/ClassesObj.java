de9bd61fce57b54065c4fdba1b1eaacd16b671be
sireesha0904 <modisireesha09@gmail.com>
2025-05-08 11:41:25 +0530

    Class and Obj

---
+++ AdvancedJava/src/classesandmethods/ClassesObj.java
@@ -0,0 +1,19 @@
+package classesandmethods;
+
+class Calculator {
+	
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+}
+
+
+public class ClassesObj {
+
+	public static void main(String[] a) {
+		Calculator obj = new Calculator();  // Object of class Calculator
+		int res = obj.add(8,6);
+		System.out.print("Addition is : "+res); 
+	}
+}
