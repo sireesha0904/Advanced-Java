c09806882ad960edd163a04981c7ff996d0a6722
sireesha0904 <modisireesha09@gmail.com>
2025-05-08 22:19:48 +0530

    GuessTheNumb

---
+++ AdvancedJava/src/Problems/GuessTheNumb.java
@@ -0,0 +1,21 @@
+package Problems;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumb {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		int attempts;
+		int randomNum = random.nextInt(1,10);
+		
+		System.out.print("Humber Guessing Game...!\n Enter the number between 1 to 10 : ");
+		
+	}
+}
