72b3547d42b31069cafe48dba3bc0522692a37fc
sireesha0904 <modisireesha09@gmail.com>
2025-05-12 14:08:06 +0530

    CircleAreaCalculator

--- AdvancedJava/src/classesandmethods/CircleAreaCalculator.java
+++ AdvancedJava/src/classesandmethods/CircleAreaCalculator.java
@@ -1,5 +1,16 @@
 package classesandmethods;
 
+public class Circle{
+	public static double CircleArea(double radius) {
+		double area = Math.PI * radius * radius;
+		return area;
+	}
+}
 public class CircleAreaCalculator {
 
+	public static void main(String[] args) {
+		double res = Circle.CircleArea(5.5);
+		
+		System.out.println("Area of Circle is : "+res);
+	}
 }
