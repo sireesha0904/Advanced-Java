f338bf3c16acdea22d5fa0f9ee6c102d39cf4f5a
sireesha0904 <modisireesha09@gmail.com>
2025-05-13 10:41:17 +0530

    Arrays

--- AdvancedJava/src/Arrays/Main.java
+++ AdvancedJava/src/Arrays/Main.java
@@ -2,4 +2,30 @@ package Arrays;
 
 public class Main {
 
+	public static void main(String[] args) {
+		
+		// Declaration 
+		int arr[];
+		
+		//Allocation 
+		arr = new int[5];
+		
+		// Initialization 
+		
+		arr[0] = 3;
+		arr[1] = 4;
+		arr[2] = 6;
+		arr[3] = 9;
+		arr[4] = 10;
+		
+		
+		// Combination 
+		
+		String branch[] = {"CSE", "ECE", "EEE", "IT", "Civil"};
+		
+		System.out.println("Number at index 2 is : "+ arr[2]);
+		System.out.println("Currently i am : "+ branch[0]);
+		
+		
+	}
 }
