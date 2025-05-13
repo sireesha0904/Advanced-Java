43cc753e0930a7fe7a4e97b8ca77e872881057d4
sireesha0904 <modisireesha09@gmail.com>
2025-05-13 09:54:44 +0530

    Banking Program

--- AdvancedJava/src/Problems/BankingProgram.java
+++ AdvancedJava/src/Problems/BankingProgram.java
@@ -1,8 +1,25 @@
 package Problems;
 
+import java.util.Scanner;
+
 public class BankingProgram {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// DECLARE A VARIABLES
+		double balance;
+		boolean isRunning = true;
+		int choice;
+		
+		System.out.println("*******************************");
+		System.out.println("Banking Program");
+		System.out.println("*******************************");
+		System.out.println("1. Show Balance");
+		System.out.println("2. Deposit");
+		System.out.println("3. Withdraw");
+		System.out.println("4. Exit");
 		
 	}
 }
