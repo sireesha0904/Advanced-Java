7ac49f0a1a15ceeb5f00e8f96162ba8530cc09c9
sireesha0904 <modisireesha09@gmail.com>
2025-05-13 11:22:55 +0530

    MultiDimArray

--- AdvancedJava/src/Arrays/MultiDimArray.java
+++ AdvancedJava/src/Arrays/MultiDimArray.java
@@ -5,11 +5,13 @@ public class MultiDimArray {
 	public static void main(String[] args) {
 		
 		int arr[][] = new int[3][4];
-		 
+		
 		for(int i=0; i<3; i++) { // Outer Loop
 			for(int j = 0; j<4; j++) { // Inner Loop
-				System.out.println(arr[i][j]);
+				arr[i][j] = (int)(Math.random() * 100);
+				System.out.print(arr[i][j] + " ");
 			}
+			System.out.println();
 		}
 	}
 }
