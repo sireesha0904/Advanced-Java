911de204970450e1d9ec6239270af4c0a2987f15
sireesha0904 <modisireesha09@gmail.com>
2025-05-13 21:12:52 +0530

    Arrays

--- AdvancedJava/src/Arrays/UserIO.java
+++ AdvancedJava/src/Arrays/UserIO.java
@@ -1,8 +1,21 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class UserIO {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		String[] colors = new String[3];
+		
+		for(int i=0; i<colors.length; i++) {
+			System.out.print("Enter colors here : ");
+			colors[i] = sc.nextLine();
+		}
+		
+		for(String col : colors) {
+			System.out.println("Colors are : "+col);
+		}
 	}
 }
