f17a9f3a2a28f614b1d4b79a909d251f18032414
sireesha0904 <modisireesha09@gmail.com>
2025-05-13 21:28:06 +0530

    search elements

--- AdvancedJava/src/Arrays/SearchElem.java
+++ AdvancedJava/src/Arrays/SearchElem.java
@@ -4,5 +4,19 @@ public class SearchElem {
 
 	public static void main(String[] args) {
 		
+		int numbers[] = {5,6,15,10,14,8,9};
+		int target = 10;
+		boolean isFound = false;
+		
+		for(int i=0; i<numbers.length; i++) {
+			if(target == numbers[i]) {
+				System.out.println("Number found at index : "+ i);
+				isFound = true;
+				break;
+			}
+		}
+		if(isFound) {
+			System.out.println("Invalid Number");
+		}
 	}
 }
