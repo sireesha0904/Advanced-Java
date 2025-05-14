11e883c60c2f33d93c15c1b17ae380054dd7dfa4
sireesha0904 <modisireesha09@gmail.com>
2025-05-14 10:37:36 +0530

    QuizeGame

---
+++ AdvancedJava/src/Arrays/QuizeGame.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class QuizeGame {
+
+	public static void main(String[] args) {
+		
+		// QUESTIONS array[]
+		// OPTIONS array[][]
+		// DECLARE VARIABLES
+		// WELCOME MESSAGE
+		// QUESTIONG(LOOP)
+		// OPTIONS
+		// GET GUESS FROM USER
+		// CHECK OUR GUESS
+		// DISPLAY FINAL SCORE
+		
+	}
+}
