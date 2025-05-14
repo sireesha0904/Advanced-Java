8ae6bef48a2bfe6ffa0e3207d1517f09a38fdad3
sireesha0904 <modisireesha09@gmail.com>
2025-05-14 10:53:23 +0530

    Quiz game

--- AdvancedJava/src/Arrays/QuizeGame.java
+++ AdvancedJava/src/Arrays/QuizeGame.java
@@ -5,14 +5,27 @@ public class QuizeGame {
 	public static void main(String[] args) {
 		
 		// QUESTIONS array[]
+		String[] ques = {"1. What is the size of an int variable in Java?", 
+				         "2. Which keyword is used to inherit a class in Java?", 
+				         "3. What is the default value of a boolean variable in Java?", 
+				         "4. Which of the following is not a Java keyword?", 
+				         "5. Which method is the entry point of any Java program?"};
 		// OPTIONS array[][]
+		String[][] options = {{"A) 2 bytes", "B) 4 bytes", "C) 8 bytes", "D) Depends on the system"},
+				              {"A) this", "B) import", "C) extends", "D) implements"},
+				              {"A) true", "B) false", "C) 0", "D) null"},
+				              {"A) static", "B) try", "C) Integer", "D) new"},
+				              {"A) start()", "B) main()", "C) run()", "D) init()"}
+				};
 		// DECLARE VARIABLES
+		String[] ans = {"B", "C", "B", "c", "B"};
 		// WELCOME MESSAGE
 		// QUESTIONG(LOOP)
 		// OPTIONS
 		// GET GUESS FROM USER
 		// CHECK OUR GUESS
 		// DISPLAY FINAL SCORE
+		
 		
 	}
 }
