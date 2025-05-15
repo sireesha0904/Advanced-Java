103e1bdcf89c49d9f628e972cd702d75f9bdfa1b
sireesha0904 <modisireesha09@gmail.com>
2025-05-15 10:16:21 +0530

    StaticMethodExample

--- AdvancedJava/src/classesandmethods/StaticMethodExample.java
+++ AdvancedJava/src/classesandmethods/StaticMethodExample.java
@@ -1,5 +1,35 @@
 package classesandmethods;
 
+class Employee {
+	int id;
+	String name;
+	static String company = "TCS";
+	
+	
+	Employee(int i, String n){
+		this.id=i;
+		this.name=n;
+	}
+	
+	public void display() {
+		System.out.println(id + " "+ name);
+	}
+	
+	public static void comp() {
+		System.out.println("Company name : "+company);
+	}
+}
 public class StaticMethodExample {
 
+	public static void main(String[] args) {
+		Employee.comp();
+		
+		Employee e1 = new Employee(1, "Sireesha");
+		Employee e2 = new Employee(1, "Gayatri");
+		Employee e3 = new Employee(1, "Kishore");
+		
+		e1.display();
+		e2.display();
+		e3.display();
+	}
 }
