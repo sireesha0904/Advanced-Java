e10597c9ca65700dd2cd22cd402edbfde22648af
sireesha0904 <modisireesha09@gmail.com>
2025-05-15 11:16:42 +0530

    Encapsulation

--- AdvancedJava/src/Encapsulation/Practice.java
+++ AdvancedJava/src/Encapsulation/Practice.java
@@ -2,23 +2,31 @@ package Encapsulation;
 
 class Human {
 	
-	private String name = "Sireesha";
-	private int age = 21;
+	private String name;
+	private int age;
 	
 	public String getName() {
 		return name;
+	}
+	
+	public void setName(String n) {
+		name = n;
 	}
 	
 	public int getAge() {
 		return age;
 	}
 	
-	
+	public void setAge(int a) {
+		age=a;
+	}
 }
 public class Practice {
 
 	public static void main(String[] args) {
 		Human obj = new Human();
+		obj.setName("Modi Sireesha");
+		obj.setAge(21);
 		
 		System.out.println(obj.getName() + " "+ obj.getAge());
 	}
