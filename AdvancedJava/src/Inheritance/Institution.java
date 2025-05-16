93ca54dd6782ddeb6f87477af62c77a4f8ce80d8
sireesha0904 <modisireesha09@gmail.com>
2025-05-16 16:47:06 +0530

    Inheritance

--- AdvancedJava/src/Inheritance/Institution.java
+++ AdvancedJava/src/Inheritance/Institution.java
@@ -3,6 +3,32 @@
 package Inheritance;
 
 
+class Person {
+	String name;
+	String address;
+	
+	void displayInfo() {
+		System.out.println("Name : "+name +"\n" + "Address : "+address);
+	}
+}
+
+class Student1 extends Person {
+	String course;
+	
+	void displayStud() {
+		System.out.println("Course is : "+course);
+	}
+	
+}
+
+class Teacher extends Person {
+	String subj;
+	
+	void displayTech() {
+		System.out.println("Subject is : "+subj);
+	}
+}
+
 public class Institution  {
 
 	public static void main(String[] args ) {
