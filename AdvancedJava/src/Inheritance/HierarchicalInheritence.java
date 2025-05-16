23b7d39f4ae7c2685877bf6d8854eff710c85e18
sireesha0904 <modisireesha09@gmail.com>
2025-05-16 16:23:36 +0530

    Hierarchical Inheritance

--- AdvancedJava/src/Inheritance/HierarchicalInheritence.java
+++ AdvancedJava/src/Inheritance/HierarchicalInheritence.java
@@ -2,29 +2,11 @@
 
 package Inheritance;
 
-class Animal {
-	void sleep() {
-		System.out.println("This Animal is Sleeping");
-	}
-}
 
-class Cats extends Animal{
-	void eat() {
-		System.out.println("This cat is eating...");
-	}
-}
-
-class Dogs extends Animal {
-	void sound() {
-		System.out.println("This Dog is Sounds like Bhow...");
-	}
-}
 public class HierarchicalInheritence {
 
 	public static void main(String[] args ) {
 		
-		Dogs d = new Dogs();
-		d.sound();
-		d.sleep();
+	
 	}
 }
