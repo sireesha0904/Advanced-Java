83f0654670b77aade212eec7cf3760702f523dad
sireesha0904 <modisireesha09@gmail.com>
2025-05-16 21:04:50 +0530

    (Method Overriding Practice)

--- AdvancedJava/src/Polymorphism/MainOverriding.java
+++ AdvancedJava/src/Polymorphism/MainOverriding.java
@@ -4,10 +4,43 @@ package Polymorphism;
 //Parent class Vehicle
 class Vehicle {
 
+	void move() {
+		System.out.println("Vehicle is moving...");
+	}
 }
+
+class Car extends Vehicle {
+	
+	void move() {
+		 System.out.println("Car is driving on the road.");
+	}
+}
+
+class Ship extends Vehicle {
+	void move() {
+		System.out.println("Ship is sailing on water.");
+	}
+}
+class Airoplane extends Vehicle {
+	 void move() {
+	        System.out.println("Airplane is flying in the sky.");
+	    }
+}
+
 
 public class MainOverriding {
  public static void main(String[] args) {
-   
+	 Vehicle v;
+	 v = new Car();
+	 v.move();
+	 
+	 v = new Ship();
+	 v.move();
+	 
+	 v= new Airoplane();
+	 v.move();
+	 
+	 
+	 
  }
 }
