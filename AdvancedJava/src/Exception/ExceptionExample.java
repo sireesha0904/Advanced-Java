9e5249bd668597eb90a48ab15789c20e3f8142f1
Modi Sireesha <modisireesha09@gmail.com>
2025-05-23 11:35:29 +0530

    Exceptions

---
+++ AdvancedJava/src/Exception/ExceptionExample.java
@@ -0,0 +1,20 @@
+package Exception;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		int j=0;
+		
+		try {
+		j = 10/i;
+		}
+		catch(Exception e) {
+			System.out.println("Something went wrong...");
+		}
+		
+		System.out.println(j);
+		
+	}
+}
