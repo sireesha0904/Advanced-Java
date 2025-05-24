f0c3bade508685525984f85a137c9c58bb8b21a3
Modi Sireesha <modisireesha09@gmail.com>
2025-05-24 11:48:56 +0530

    MultiThreading

--- AdvancedJava/src/MultiThreading/Demo.java
+++ AdvancedJava/src/MultiThreading/Demo.java
@@ -1,8 +1,34 @@
 package MultiThreading;
 
+class A extends Thread{
+	
+	public void run() {
+		for(int i=0; i<=5; i++) {
+			System.out.println("Inside Class A");
+		}
+		
+	}
+}
+
+class B extends Thread{
+    public void run() {
+    	for(int i=0; i<=5; i++) {
+			System.out.println("Inside Class B");
+		}
+	}
+}
 public class Demo {
 
 	public static void main(String[] args) {
+		
+		A t1 = new A();
+		B t2 = new B();
+		
+		t2.setPriority(3);
+		t1.setPriority(4);
+		
+		t1.start();
+		t2.start();
 		
 	}
 }
