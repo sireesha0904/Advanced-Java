002a25c3a1708a94fe1da8ff15d8b0291e2f8e8f
Modi Sireesha <modisireesha09@gmail.com>
2025-05-25 12:55:12 +0530

    List

--- AdvancedJava/src/CollectionEx/ListInterfaceEx.java
+++ AdvancedJava/src/CollectionEx/ListInterfaceEx.java
@@ -1,18 +1,25 @@
 package CollectionEx;
 
 import java.util.ArrayList;
-import java.util.Collection;
-//import java.util.List;
+//import java.util.Collection;
+import java.util.List;
 
 public class ListInterfaceEx {
 
 	public static void main(String[] args) {
 		
-		Collection<Integer> list = new ArrayList<>();.
+		List<Integer> list = new ArrayList<>();
 		list.add(5);
 		list.add(10);
 		list.add(2);
 		
-		System.out.println(list);
+		for(int l : list) {
+			System.out.println(l);
+		}
+		
+		System.out.println("Value at index 0 : "+list.get(0));
+		list.add(1,15);
+		System.out.println("After Insertion : "+list);
+		
 	}
 }
