891eef5d56ca45a82a93c7285ffd53b466969161
Modi Sireesha <modisireesha09@gmail.com>
2025-05-25 12:46:38 +0530

    Collections

---
+++ AdvancedJava/src/CollectionEx/ListInterfaceEx.java
@@ -0,0 +1,18 @@
+package CollectionEx;
+
+import java.util.ArrayList;
+import java.util.Collection;
+//import java.util.List;
+
+public class ListInterfaceEx {
+
+	public static void main(String[] args) {
+		
+		Collection<Integer> list = new ArrayList<>();.
+		list.add(5);
+		list.add(10);
+		list.add(2);
+		
+		System.out.println(list);
+	}
+}
