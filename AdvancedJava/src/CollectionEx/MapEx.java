8af503a31a9f03a1a66ed99c24f65d005b66b7b8
Modi Sireesha <modisireesha09@gmail.com>
2025-05-26 12:10:37 +0530

    Map Implementation

--- AdvancedJava/src/CollectionEx/MapEx.java
+++ AdvancedJava/src/CollectionEx/MapEx.java
@@ -1,6 +1,23 @@
 package CollectionEx;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapEx {
 
-	
+	public static void main(String[] args) {
+		
+		Map<String, String> country = new HashMap<>();
+		
+		// Add Elements
+		country.put("India", "New Delhi");
+		country.put("USA", "Washington DC");
+		country.put("France", "Paris");
+		
+		//Access values
+		System.out.println("Captial of India : "+country.get("India"));
+		System.out.println(country.entrySet());
+		
+		System.out.println("Has Japan? " + country.containsKey("Japan"));
+	}
 }
