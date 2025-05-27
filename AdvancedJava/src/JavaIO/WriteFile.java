abc9b1fe34a7543c4d8d0050f350cd6f88b3064d
Modi Sireesha <modisireesha09@gmail.com>
2025-05-27 19:24:31 +0530

    WriteFile Implementation

--- AdvancedJava/src/JavaIO/WriteFile.java
+++ AdvancedJava/src/JavaIO/WriteFile.java
@@ -1,8 +1,17 @@
 package JavaIO;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WriteFile {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+	FileWriter fw = new FileWriter("output.txt");
+	
+	fw.write("Hello Java I/O");
+	fw.close();
+	
+	System.out.println("Sucessfully File Written...");
 	}
 }
