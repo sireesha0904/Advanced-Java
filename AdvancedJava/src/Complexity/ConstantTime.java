1f72269bcac82277bac680980f2c9a46253c5d9c
Modi Sireesha <modisireesha09@gmail.com>
2025-06-01 11:46:09 +0530

    Constant time Complexity implementation

---
+++ AdvancedJava/src/Complexity/ConstantTime.java
@@ -0,0 +1,10 @@
+package Complexity;
+
+public class ConstantTime {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,6};
+		System.out.println("Accessing index at 2 in an Array : "+ arr[2]);
+	}
+	
+}
--- AdvancedJava/src/Complexity/Main.java
+++
@@ -1,6 +0,0 @@
-package Complexity;
-
-public class Main {
-
-	
-}
