4054f7dfe1cdc93ff8d6e5f5584445228c52390f
Modi Sireesha <modisireesha09@gmail.com>
2025-06-04 15:35:50 +0530

    Delete the first element

--- AdvancedJava/src/DSA/Arrayproblems/DeleteAtFirstIndex.java
+++ AdvancedJava/src/DSA/Arrayproblems/DeleteAtFirstIndex.java
@@ -4,5 +4,23 @@ public class DeleteAtFirstIndex {
 
 	public static void main(String[] args) {
 		
+		int[] arr = {10,20,30,40,50};
+		int n = arr.length;
+		
+		int[] newArr = new int[n-1];
+		
+		for(int i=0, k = 0; i<n; i++) {
+			if(i == 0) {
+				continue;
+			}
+			newArr[k] = arr[i];
+			k++;
+		}
+		
+		System.out.print("Afeter delete element at 1st index : ");
+		for(int num : newArr) {
+			
+			System.out.print(num + " ");	
+		}
 	}
 }
