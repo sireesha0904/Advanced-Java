20cda4244dd96809c46822f8712b0e6e6f9f8ab2
Modi Sireesha <modisireesha09@gmail.com>
2025-06-04 16:12:07 +0530

    Reverse an Array

--- AdvancedJava/src/DSA/Arrayproblems/ReverseAnArray.java
+++ AdvancedJava/src/DSA/Arrayproblems/ReverseAnArray.java
@@ -2,4 +2,28 @@ package DSA.Arrayproblems;
 
 public class ReverseAnArray {
 
+	public static void main(String[] args) {
+		
+		int[] arr  = {10,20,30,40,50};
+		int n = arr.length;
+		
+		int start = 0;
+		int end = n-1;
+		
+		while(start >= end) {
+			
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			
+			start ++;
+			end --;
+		}
+		
+		 // Output the reversed array
+        System.out.print("Reversed Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+	}
 }
