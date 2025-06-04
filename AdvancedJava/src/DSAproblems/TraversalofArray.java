aa3f495aa73bcb37b1626ac39fe8b33b39aa73ac
Modi Sireesha <modisireesha09@gmail.com>
2025-06-04 11:12:41 +0530

    Traversal of an Array Implementation

--- AdvancedJava/src/DSAproblems/TraversalofArray.java
+++ AdvancedJava/src/DSAproblems/TraversalofArray.java
@@ -2,4 +2,21 @@ package DSAproblems;
 
 public class TraversalofArray {
 
+	public static void main(String[] args) {
+		
+		int[] arr = {3,4,1,2,6};
+		int n = arr.length;
+		
+		//forward traversal 
+		System.out.print("Forward Traversal of an Array : ");
+		for(int i =0; i<n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+		 // Reverse Traversal
+		System.out.print("\nReverse Traversal of an Array : ");
+		for(int i = n-1; i>=0; i--) {
+			System.out.print(arr[i] + " ");
+		}
+	}
 }
