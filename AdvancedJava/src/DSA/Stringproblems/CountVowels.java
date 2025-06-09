925c664aed8ba88c3ee20414b572f2018bbe7709
Modi Sireesha <modisireesha09@gmail.com>
2025-06-09 19:36:05 +0530

    Count Vowels and Consonants

--- AdvancedJava/src/DSA/Stringproblems/CountVowels.java
+++ AdvancedJava/src/DSA/Stringproblems/CountVowels.java
@@ -3,6 +3,22 @@ package DSA.Stringproblems;
 public class CountVowels {
 
 	public static void main(String[] args) {
+		String str = "Hello World";
+		int vowels = 0, consonants  = 0;
+		str = str.toLowerCase();
+		
+		for(int i =0; i<str.length(); i++) {
+			char ch = str.charAt(i);
+			
+			if(Character.isLetter(ch)) {
+				if("aeiou".indexOf(ch) != -1) {
+					vowels++;
+				} else {
+					consonants ++;
+				}
+			}
+		}
+		System.out.println("Vowels: " + vowels + ", Consonants: " + consonants );
 		
 
 	}
