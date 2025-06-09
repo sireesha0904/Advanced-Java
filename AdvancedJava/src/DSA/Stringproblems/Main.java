8810c47a8203b67f5cbe5246041786d33337cd09
Modi Sireesha <modisireesha09@gmail.com>
2025-06-09 18:58:06 +0530

    String Basics Implementation

--- AdvancedJava/src/DSA/Stringproblems/Main.java
+++ AdvancedJava/src/DSA/Stringproblems/Main.java
@@ -3,10 +3,35 @@ package DSA.Stringproblems;
 public class Main {
 
 	public static void main(String[] args) {
-		String str = "Hello java";
-		String str2 = new String("Hello Strings");
+		String str = "Hello jAva";
+		String str2 = new String("Hello Strings"); // stored in heap memory
+		
+		
+		int leng = str.length();
+		String substr = str.substring(1,5);
+		String replace = str.replace("java", "Programming");
+		
 		
 		System.out.println(str);
 		System.out.println(str2);
+		System.out.println(leng);
+		System.out.println(substr);
+		System.out.println(str.charAt(4));
+		System.out.println(replace);
+		
+		
+		System.out.println("\nPrint Each Character in a String");
+		
+		for(int i=0; i<leng; i++) {
+			System.out.println(str.charAt(i));
+		}
+		
+		
+		String s = "SIREESHA";
+		StringBuilder sb = new StringBuilder(s);
+		sb.reverse();
+		System.out.println("\nReverse a String - " + sb);
+		
+		
 	}
 }
