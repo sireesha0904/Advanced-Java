b1a49b862d89b1730b9c8ab844681ec7b1f0b5c3
Modi Sireesha <modisireesha09@gmail.com>
2025-06-09 18:47:00 +0530

    Basic of Strings

--- AdvancedJava/src/DSA/Stringproblems/Main.java
+++ AdvancedJava/src/DSA/Stringproblems/Main.java
@@ -2,4 +2,11 @@ package DSA.Stringproblems;
 
 public class Main {
 
+	public static void main(String[] args) {
+		String str = "Hello java";
+		String str2 = new String("Hello Strings");
+		
+		System.out.println(str);
+		System.out.println(str2);
+	}
 }
