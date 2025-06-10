317462f9136e6bde4b87b4ee4f74f84a430e785f
Modi Sireesha <modisireesha09@gmail.com>
2025-06-10 22:19:00 +0530

    Problem Statement: Valid Palindrome with Symbols

---
+++ AdvancedJava/src/DSA/Stringproblems/ValidPalindrome.java
@@ -0,0 +1,32 @@
+package DSA.Stringproblems;
+
+public class ValidPalindrome {
+
+	public static boolean isPalindrome(String s) {
+		s = s.toLowerCase();
+		
+		int start = 0, end = s.length()-1;
+		
+		while(start < end) {
+			
+			while(start < end && !Character.isLetterOrDigit(s.charAt(start))) {
+				start ++;
+			}
+			
+			while(start < end && !Character.isLetterOrDigit(s.charAt(end))) {
+				end --;
+			}
+			
+			if(s.charAt(start) != s.charAt(end)) {
+				return false;
+			}
+		}
+		return true;
+		
+	}
+	public static void main(String[] args) {
+		
+		System.out.println(isPalindrome("No lemon, no melon"));   
+	}
+
+}
