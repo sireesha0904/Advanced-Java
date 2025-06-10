d71dfcce26606958dadc4fd92d270dfc3cd32441
Modi Sireesha <modisireesha09@gmail.com>
2025-06-10 15:45:55 +0530

    check if a string is a palindrome

--- AdvancedJava/src/DSA/Stringproblems/CheckPalindrome.java
+++ AdvancedJava/src/DSA/Stringproblems/CheckPalindrome.java
@@ -2,6 +2,7 @@ package DSA.Stringproblems;
 
 class Palindrome{
 	public static boolean isPalindrome(String s) {
+	 s = s.toLowerCase();
 		int left = 0; 
 		int right = s.length()-1;
 		while(left < right) {
@@ -20,7 +21,7 @@ public class CheckPalindrome {
 	
 	public static void main(String[] args) {
 		System.out.println("Sireesha is a palindrome - "+ Palindrome.isPalindrome("Sireesha"));
-		System.out.println("madam is a palindrome - "+ Palindrome.isPalindrome("madam"));
+		System.out.println("madam is a palindrome - "+ Palindrome.isPalindrome("Madam"));
 		System.out.println("racecar is a palindrome - "+ Palindrome.isPalindrome("racecar"));
 		
 	}
