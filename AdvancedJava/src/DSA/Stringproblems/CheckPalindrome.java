7e08597eb0f5cf9922b02266f0b9aefe720556bf
Modi Sireesha <modisireesha09@gmail.com>
2025-06-10 15:42:33 +0530

    check if a string is a palindrome

--- AdvancedJava/src/DSA/Stringproblems/CheckPalindrome.java
+++ AdvancedJava/src/DSA/Stringproblems/CheckPalindrome.java
@@ -4,8 +4,8 @@ class Palindrome{
 	public static boolean isPalindrome(String s) {
 		int left = 0; 
 		int right = s.length()-1;
-		
 		while(left < right) {
+			
 			if(s.charAt(left) != s.charAt(right)) {
 				return false;
 			}
@@ -19,9 +19,9 @@ public class CheckPalindrome {
 
 	
 	public static void main(String[] args) {
-		System.out.println(s +"is a palindrome "+ Palindrome.isPalindrome("Sireesha"));
-		System.out.println(s +"is a palindrome "+ Palindrome.isPalindrome("madam"));
-		System.out.println(s +"is a palindrome "+ Palindrome.isPalindrome("racecar"));
+		System.out.println("Sireesha is a palindrome - "+ Palindrome.isPalindrome("Sireesha"));
+		System.out.println("madam is a palindrome - "+ Palindrome.isPalindrome("madam"));
+		System.out.println("racecar is a palindrome - "+ Palindrome.isPalindrome("racecar"));
 		
 	}
 }
