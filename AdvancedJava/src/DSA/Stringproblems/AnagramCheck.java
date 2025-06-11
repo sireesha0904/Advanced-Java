d18de4d05b23c7f4fabd1426723ef20ffed16c47
Modi Sireesha <modisireesha09@gmail.com>
2025-06-11 15:14:40 +0530

    Anagrams & Frequency-Based Problems

---
+++ AdvancedJava/src/DSA/Stringproblems/AnagramCheck.java
@@ -0,0 +1,33 @@
+package DSA.Stringproblems;
+
+public class AnagramCheck {
+
+	public static boolean isAnagram(String s, String t) {
+		
+		if(s.length() != t.length()) {
+			return false;
+		}
+		
+		int[] frequncy = new int[26];
+		for(int i=0; i<s.length(); i++) {
+			char ch = s.charAt(i);
+			frequncy[ch - 'a'] ++;
+		}
+		
+		for(int i=0; i<s.length(); i++) {
+			char ch = t.charAt(i);
+			frequncy[ch - 'a'] --;
+		}
+		
+		for(int count : frequncy) {
+			if(count != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args) {
+		System.out.println(isAnagram("silent", "listen"));
+		System.out.println(isAnagram("madam", "women"));
+	}
+}
