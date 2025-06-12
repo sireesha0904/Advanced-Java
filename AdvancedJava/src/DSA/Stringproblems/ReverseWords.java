5b05fbe78e8397e706e14dd9bbe74247242e5cc3
Modi Sireesha <modisireesha09@gmail.com>
2025-06-12 11:27:15 +0530

    Reverse Words in a String

--- AdvancedJava/src/DSA/Stringproblems/ReverseWords.java
+++ AdvancedJava/src/DSA/Stringproblems/ReverseWords.java
@@ -1,5 +1,25 @@
 package DSA.Stringproblems;
 
 public class ReverseWords {
-
+public static String reversWords(String s) {
+	
+	String[] words =s.trim().split("\\s+");
+	int start = 0, end = words.length-1;
+	
+	while(start < end) {
+		String temp = words[start];
+		words[start] = words[end];
+		words[end] = temp;
+		
+		start++;
+		end --;
+	}
+	
+	return String.join(" ", words);
+}
+	
+	public static void main(String[] args) {
+		String input = " i love coding ";
+		System.out.println(reversWords(input));
+	}
 }
