54e72af860fef202156e804179ed24c9bf00cbbe
Modi Sireesha <modisireesha09@gmail.com>
2025-06-12 10:44:01 +0530

    Reverse a String

--- AdvancedJava/src/DSA/Stringproblems/ReverseString.java
+++ AdvancedJava/src/DSA/Stringproblems/ReverseString.java
@@ -5,7 +5,18 @@ public class ReverseString {
 	public static String reverse(String s) {
 		
 		char[] ch = s.toCharArray();
+		int start = 0, end = s.length()-1;
 		
+		while(start < end) {
+			
+			char temp = ch[start];
+			ch[start]=ch[end];
+			ch[end]=temp;
+			
+			start ++;
+			end --;
+		}
+		return new String(ch);
 		
 	}
 	public static void main(String[] args) {
