629b63689decf111e8b612d0bb2f969419e8078a
Modi Sireesha <modisireesha09@gmail.com>
2025-06-13 14:24:14 +0530

    Longest substring using sliding window technique

--- AdvancedJava/src/DSA/Stringproblems/LongestSubString.java
+++ AdvancedJava/src/DSA/Stringproblems/LongestSubString.java
@@ -1,5 +1,32 @@
 package DSA.Stringproblems;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubString {
 
+	public int Longestsubstring(String s) {
+		Set<Character> set = new HashSet<>();
+		int left =0;
+		int maxLen = 0;
+		
+		for(int right = 0; right < s.length(); right++) {
+			char ch = s.charAt(right);
+			
+			while(set.contains(ch)) {
+				set.remove(left);
+				left++;
+			}
+			set.add(ch);
+			maxLen = Math.max(maxLen, right - left +1);
+		}
+		return maxLen;
+	}
+	public static void main(String[] args) {
+		
+		LongestSubString ls = new LongestSubString();
+		String inp = "ababac";
+		System.out.println(ls.Longestsubstring(inp));
+		
+	}
 }
