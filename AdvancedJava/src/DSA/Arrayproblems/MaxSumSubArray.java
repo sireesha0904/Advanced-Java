c6196e0c5936bffd3ec4f5a64e4b89ed8f1d1ce4
Modi Sireesha <modisireesha09@gmail.com>
2025-06-13 15:01:57 +0530

    Sliding window Technique

--- AdvancedJava/src/DSA/Arrayproblems/MaxSumSubArray.java
+++ AdvancedJava/src/DSA/Arrayproblems/MaxSumSubArray.java
@@ -2,9 +2,27 @@ package DSA.Arrayproblems;
 
 public class MaxSumSubArray {
 
+	public static int max(int[] arr,int k) {
+	int windowSlide =0, maxSum = 0;
+	
+	for(int i=0; i < k; i++) {
+		windowSlide += arr[i];
+	}
+	maxSum = windowSlide;
+	
+	for(int i=k; i<arr.length; i++) {
+		windowSlide += arr[i];
+		windowSlide -= arr[i-k];
+		maxSum = Math.max(maxSum, windowSlide);
+	}
+	return maxSum;
+	}
+	
 	public static void main(String[] args) {
-		
 
+		int[] inp = {2,1,3,4};
+		System.out.println("Max sum of sub array elements with length 2 : "+max(inp, 2));
+		
 	}
 
 }
