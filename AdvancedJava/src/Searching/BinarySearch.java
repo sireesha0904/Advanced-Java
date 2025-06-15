fb55de1193058e273e5c2d855d21d7dcf5bad83b
Modi Sireesha <modisireesha09@gmail.com>
2025-06-15 11:45:32 +0530

    Binary Search

--- AdvancedJava/src/Searching/BinarySearch.java
+++ AdvancedJava/src/Searching/BinarySearch.java
@@ -2,9 +2,35 @@ package Searching;
 
 public class BinarySearch {
 
-	
+	public static int binarySearch(int arr[], int key) {
+		int low =0; 
+		int high = arr.length-1;
+		
+		while(low<=high) {
+			int mid = low + (high - low)/2;
+			
+			if(arr[mid] == key) {
+				return mid;
+			}
+			else if(arr[mid]<key) {
+				low = mid+1;
+			}
+			else {
+				high = mid -1;
+			}
+		}
+		return -1;
+	}
 	
 	public static void main(String[] args) {
 		
+		int[] a = {2,4,5,9,8,10,12};
+		int result = binarySearch(a,10);
+		
+		if(result != -1) {
+			  System.out.println("Element found at index: " + result);
+		}
+		else 
+			  System.out.println("Element not found ");
 	}
 }
