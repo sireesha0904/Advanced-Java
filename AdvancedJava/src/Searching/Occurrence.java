ac0f7669159ca8b01006639d3295fe207f7f9f44
Modi Sireesha <modisireesha09@gmail.com>
2025-06-16 09:23:37 +0530

    First and Last Occurrence of element in sorted array

--- AdvancedJava/src/Searching/Occurrence.java
+++ AdvancedJava/src/Searching/Occurrence.java
@@ -2,7 +2,49 @@ package Searching;
 
 public class Occurrence {
 
+	public static int firstOcc(int[] arr, int key) {
+		int low =0;
+		int high= arr.length-1;
+		int first = -1;
+		
+		while(low<=high) {
+			int mid = low+(high - low)/2;
+			
+			if(arr[mid]==key) {
+				first = mid;
+				high = mid-1;
+			}
+			else if(arr[mid]<key) {
+				low=mid+1;
+			}
+			else {
+				high = mid-1;
+			}
+		}
+		return first;
+	}
 	
+	public static int lastOcc(int[] arr, int key) {
+		int low =0;
+		int high= arr.length-1;
+		int last = -1;
+		
+		while(low<=high) {
+			int mid = low+(high - low)/2;
+			
+			if(arr[mid]==key) {
+				last = mid;
+				low=mid+1;
+			}
+			else if(arr[mid]<key) {
+				low=mid+1;
+			}
+			else {
+				high = mid-1;
+			}
+		}
+		return last;
+	}
 	public static void main(String[] args) {
 		
 	}
