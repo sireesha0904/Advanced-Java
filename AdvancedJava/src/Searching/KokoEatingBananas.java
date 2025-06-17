2a38fa77bd5db7564ba79125b73917da0a8a3a1c
Modi Sireesha <modisireesha09@gmail.com>
2025-06-17 13:31:03 +0530

    Binary Search on Answer space

--- AdvancedJava/src/Searching/KokoEatingBananas.java
+++ AdvancedJava/src/Searching/KokoEatingBananas.java
@@ -10,9 +10,15 @@ public class KokoEatingBananas {
 		while(low<=high) {
 			int mid = low + (high - low)/2;
 			
-			if()
+			if(isCanFinish(piles, h, mid)) {
+				result = mid;
+				high = mid - 1;
+			}
+			else {
+				low = mid + 1;
+			}
 		}
-		
+		return result;
 	
 	}
 	
