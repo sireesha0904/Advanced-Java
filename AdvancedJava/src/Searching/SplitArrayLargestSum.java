423860d6d1e97a27dac3ae27fe32719150482d7d
Modi Sireesha <modisireesha09@gmail.com>
2025-06-18 10:44:07 +0530

    SplitArrayLargestSum  Problem

--- AdvancedJava/src/Searching/SplitArrayLargestSum.java
+++ AdvancedJava/src/Searching/SplitArrayLargestSum.java
@@ -2,4 +2,28 @@ package Searching;
 
 public class SplitArrayLargestSum {
 
+	static int splitArray(int[] arr, int k) {
+		int low = getMax(arr);
+		int high = getSum(arr);
+		int result = high;
+	}
+	
+	static int getMax(int[] arr) {
+		int max = Integer.MIN_VALUE;
+		for(int a : arr) {
+			max = Math.max(a, max);
+		}
+		return max;
+	}
+	
+	static int getSum(int[] arr) {
+		int sum =0;
+		for(int a : arr) {
+			sum += a;
+		}
+		return sum;
+	}
+	public static void main(String[] args) {
+		
+	}
 }
