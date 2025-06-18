3c5b200d7a809913ae826e4cd4b8eb4d079f4fe0
Modi Sireesha <modisireesha09@gmail.com>
2025-06-18 16:44:20 +0530

    Find the element that appears once in sorted array

---
+++ AdvancedJava/src/Searching/FindUniqueElem.java
@@ -0,0 +1,30 @@
+package Searching;
+
+public class FindUniqueElem {
+
+	 int uniqueEle(int[] arr, int k) {
+		
+		int low =0; int high = arr.length - 1;
+		while(low <= high) {
+			int mid = low + (high - low)/2;
+			
+			if(arr[mid] % 2 == 1) mid --;
+			
+			if(arr[mid] == arr[mid+1]) {
+				low = mid + 2;
+			}
+			else {
+				high = mid;
+			}
+		}
+		return arr[low];
+	}
+	public static void main(String[] args) {
+		FindUniqueElem uni = new FindUniqueElem();
+		int[] arr = {1, 1, 2, 2, 3, 3, 4, 4, 5};
+		int ele = 5;
+		int result = uni.uniqueEle(arr, ele);
+		System.out.println("Unique element from array is "+result);
+	}
+}
+
