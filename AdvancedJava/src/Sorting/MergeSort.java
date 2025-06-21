04056ef9e5f48b21e04dd6142ca967cc839605b5
Modi Sireesha <modisireesha09@gmail.com>
2025-06-21 21:43:54 +0530

    Merge Sort

--- AdvancedJava/src/Sorting/MergeSort.java
+++ AdvancedJava/src/Sorting/MergeSort.java
@@ -20,10 +20,11 @@ public class MergeSort {
 		int n2 = r - m; // length of right sub array 
 		
 		
-		//Temporary 
-		int[] L = new int[n1];
-		int[] R = new int[n2];
+		//Temporary array length for both parts 
+		int[] L = new int[n1]; // left length
+		int[] R = new int[n2]; // right length
 		
+		//copy values from arr to left and right;
 		for(int i=0; i<n1; i++) {
 			L[i] = arr[l+i];
 		}
@@ -31,11 +32,44 @@ public class MergeSort {
 			R[j] = arr[m + 1 + j];
 		}
 		
+		// merge two temp arrays into single arr[]
+		
+		int i =0, j =0, k=l;
+		while(i < n1 && j < n2) {
+			if(L[i] < R[j]) {
+				arr[k] = L[i];
+				i++;
+			}
+			else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+		
+		// handle leftovers 
+		
+		while(i<n1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+		
+		while(j < n2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
 	}
 	
 	public static void main(String[] args) {
 		
+		 int[] arr = {6, 3, 9, 5, 2};
+	        mergeSort(arr, 0, arr.length - 1);
 
+	        for (int val : arr) {
+	            System.out.print(val + " ");
+	        }
 	}
 
 }
