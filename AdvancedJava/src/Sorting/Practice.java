2759900329279fe77cf42acb415ada5b5b32fff7
Modi Sireesha <modisireesha09@gmail.com>
2025-06-23 20:40:43 +0530

    Practice Sorting problems

--- AdvancedJava/src/Sorting/Practice.java
+++ AdvancedJava/src/Sorting/Practice.java
@@ -1,7 +1,27 @@
 package Sorting;
 
 public class Practice {
-	public static void main(String[] args) {
+	static void bubbleSort(int[] arr) {
+		int n = arr.length;
 		
+		for(int i=0; i<n; i++) {
+			for(int j =0; j < n-i-1; j++) {
+				
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+					
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr = {5,2,4,3,7,9,0};
+		bubbleSort(arr);
+		System.out.println("Sorted Arrat - ");
+		for(int a : arr) {
+			System.out.print(a);
+		}
 	}
 }
