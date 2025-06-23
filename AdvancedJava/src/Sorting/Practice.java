f3b7044a82676b16723e99da4427668f06f26fc1
Modi Sireesha <modisireesha09@gmail.com>
2025-06-23 20:43:18 +0530

    Practice Sorting problems - counting sort

--- AdvancedJava/src/Sorting/Practice.java
+++ AdvancedJava/src/Sorting/Practice.java
@@ -1,24 +1,13 @@
 package Sorting;
 
 public class Practice {
-	static void bubbleSort(int[] arr) {
-		int n = arr.length;
+	static void countingSort(int[] arr) {
 		
-		for(int i=0; i<n; i++) {
-			for(int j =0; j < n-i-1; j++) {
-				
-				if(arr[j] > arr[j+1]) {
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-					
-				}
-			}
-		}
 	}
+
 	public static void main(String[] args) {
-		int[] arr = {5,2,4,3,7,9,0};
-		bubbleSort(arr);
+		int[] arr = {5,5,2,2,2,4,3,3,7,7,9,0};
+		countingSort(arr);
 		System.out.print("Sorted Array - ");
 		for(int a : arr) {
 			System.out.print(a + " ");
