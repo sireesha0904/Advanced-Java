0043467719a0809fd68d449f59cbcee1a746a6d6
Modi Sireesha <modisireesha09@gmail.com>
2025-07-01 09:19:33 +0530

    Linked List Implementation

---
+++ AdvancedJava/src/DSA/LinkedLists/LinkedListExample.java
@@ -0,0 +1,32 @@
+package DSA.LinkedLists;
+
+class Node {
+	int data;
+	Node next;
+	
+	Node(int data){
+		this.data = data;
+		this.next = null;
+	}
+}
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		
+		Node first = new Node(10);
+		Node sec = new Node(20);
+		Node third = new Node(30);
+		
+		first.next = sec;
+		sec.next = third;
+		
+		Node head = first;
+		
+		Node current = first;
+		while(current != null) {
+			System.out.print(current.data + " -> ");
+			current = current.next;
+		}
+		System.out.print("null");
+	}
+}
