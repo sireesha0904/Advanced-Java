39c7caf8e3befbb6fd857144cdfb97ecdcc042df
Modi Sireesha <modisireesha09@gmail.com>
2025-07-02 21:21:04 +0530

    Reversed Linked List

--- AdvancedJava/src/DSA/LinkedLists/ReverseLinkedList.java
+++ AdvancedJava/src/DSA/LinkedLists/ReverseLinkedList.java
@@ -1,52 +1,53 @@
-package DSA.LinkedLists;
-
-class RNode {
-	int data;
-	RNode next;
-	RNode(int data){
-		this.data = data;
-		this.next = null;
-	}
-}
-
-class ReversedLinkedList{
+	package DSA.LinkedLists;
 	
-	static RNode listReversed(RNode head) {
-		RNode prev = null;
-		RNode curr = head;
+	class RNode {
+		int data;
+		RNode next;
+		RNode(int data){
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	class ReversedLinkedList{
 		
-		while(curr != head) {
-			RNode next = curr.next;
-			curr.next = prev;
+		static RNode listReversed(RNode head) {
+			RNode prev = null;
+			RNode curr = head;
 			
-			prev = curr;
-			curr = next;
+			while(curr != null) {
+				RNode next = curr.next;
+				curr.next = prev;
+				
+				prev = curr;
+				curr = next;
+			}
+			return prev;
 		}
-		return prev;
-	}
-	
-	static void printList(RNode head) {
-		RNode temp = head;
-		while(temp != head) {
-			System.out.print(temp.data + "->");
-			temp = temp.next;
+		
+		static void printList(RNode head) {
+			RNode temp = head;
+			while(temp != null) {
+				System.out.print(temp.data + "->");
+				temp = temp.next;
+			}
+			System.out.println("null");
 		}
-		System.out.println("null");
+		
 	}
-	
-}
-public class ReverseLinkedList {
-	
-	public static void main(String[] args) {
-		RNode head = new RNode(10);
-		head.next = new RNode(20);
-		head.next.next = new RNode(30);
+	public class ReverseLinkedList {
 		
-		System.out.println("Original List : ");
-		ReversedLinkedList.printList(head);
-		
-		
-		head = ReversedLinkedList.listReversed(head);
-		System.out.println("\nReversed List : ");
+		public static void main(String[] args) {
+			RNode head = new RNode(10);
+			head.next = new RNode(20);
+			head.next.next = new RNode(30);
+			
+			System.out.println("Original List : ");
+			ReversedLinkedList.printList(head);
+			
+			
+			head = ReversedLinkedList.listReversed(head);
+			System.out.println("\nReversed List : ");
+			ReversedLinkedList.printList(head);
+		}
 	}
-}
