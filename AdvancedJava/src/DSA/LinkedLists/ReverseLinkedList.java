50d596cd9c22fec887ee0654b0b962c644c3d7de
Modi Sireesha <modisireesha09@gmail.com>
2025-07-02 21:11:20 +0530

    Reversed Linked List

--- AdvancedJava/src/DSA/LinkedLists/ReverseLinkedList.java
+++ AdvancedJava/src/DSA/LinkedLists/ReverseLinkedList.java
@@ -36,6 +36,15 @@ class ReversedLinkedList{
 public class ReverseLinkedList {
 	
 	public static void main(String[] args) {
+		Node head = new Node(10);
+		head.next = new Node(20);
+		head.next.next = new Node(30);
 		
+		System.out.println("Original List : ");
+		ReversedLinkedList.printList(head);
+		
+		
+		head = ReversedLinkedList.listReversed(head);
+		System.out.println("\nReversed List : ");
 	}
 }
