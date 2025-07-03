27f6d4e30eef2dfc2b32a43fe59fffd351ec80f6
Modi Sireesha <modisireesha09@gmail.com>
2025-07-03 21:23:03 +0530

    InsertAtBeginningExistingList

--- AdvancedJava/src/DSA/LinkedLists/InsertAtBeginningExistingList.java
+++ AdvancedJava/src/DSA/LinkedLists/InsertAtBeginningExistingList.java
@@ -1,5 +1,30 @@
 package DSA.LinkedLists;
 
+class INode{
+	int data;
+	INode next;
+	
+	INode(int data){
+		this.data = data;
+		this.next = null;
+	}
+}
 public class InsertAtBeginningExistingList {
 
+	static INode insertNode(INode head, int value) {
+		INode newNode = new INode(value);
+		newNode.next = head;
+		return newNode;
+		
+	}
+	static void printList(INode head) {
+		INode curr = head;
+		while(curr != null) {
+			System.out.print(curr.data + " -> ");
+		}
+		System.out.println("null");
+	}
+	public static void main(String[] args) {
+		
+	}
 }
