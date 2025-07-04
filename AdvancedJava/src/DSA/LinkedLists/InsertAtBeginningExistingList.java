85ff61cd49ea99434587cbebcebb489a8d712f2e
Modi Sireesha <modisireesha09@gmail.com>
2025-07-04 10:31:45 +0530

    Insert Operation on Linked List

--- AdvancedJava/src/DSA/LinkedLists/InsertAtBeginningExistingList.java
+++ AdvancedJava/src/DSA/LinkedLists/InsertAtBeginningExistingList.java
@@ -21,10 +21,21 @@ public class InsertAtBeginningExistingList {
 		INode curr = head;
 		while(curr != null) {
 			System.out.print(curr.data + " -> ");
+			curr = curr.next;
 		}
 		System.out.println("null");
 	}
+	
 	public static void main(String[] args) {
+		INode head = new INode(10);
+		head.next = new INode(20);
+		head.next.next = new INode(30);
 		
+		System.out.println("Original List is ");
+		printList(head);
+		
+		head = insertNode(head, 5);
+		System.out.println("\nNew List is ");
+		printList(head);
 	}
 }
