319b9f8545dd0db5e64f9603a16b4a44284bf46f
Modi Sireesha <modisireesha09@gmail.com>
2025-07-05 10:54:17 +0530

    Detect Cycle in a Linked List

--- AdvancedJava/src/DSA/LinkedLists/DetectCycle.java
+++ AdvancedJava/src/DSA/LinkedLists/DetectCycle.java
@@ -11,7 +11,31 @@ class DNode {
 }
 public class DetectCycle {
 
-	public static void main(String[] args) {
+	static boolean cycleDitect(DNode head) {
+		DNode slow = head , fast = head;
 		
+		if (head == null || head.next == null) return false;
+		
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			
+			if(fast == slow) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	public static void main(String[] args) {
+		DNode head = new DNode(1);
+		head.next = new DNode(2);
+		head.next.next = new DNode(3);
+		head.next.next.next = new DNode(4);
+		
+		
+		head.next.next.next = head.next;
+		
+		System.out.println("Has Cycle " + cycleDitect(head));
 	}
 }
