adb872c4ac8865c652fc9b9ae1162faa63b8f7cc
Modi Sireesha <modisireesha09@gmail.com>
2025-07-05 10:48:26 +0530

    Detect Cycle in a Linked List

--- AdvancedJava/src/DSA/LinkedLists/DetectCycle.java
+++ AdvancedJava/src/DSA/LinkedLists/DetectCycle.java
@@ -1,5 +1,17 @@
 package DSA.LinkedLists;
 
+class DNode {
+	int data;
+	DNode next;
+	
+	DNode(int data){
+		this.data = data;
+		this.next = null;
+	}
+}
 public class DetectCycle {
 
+	public static void main(String[] args) {
+		
+	}
 }
