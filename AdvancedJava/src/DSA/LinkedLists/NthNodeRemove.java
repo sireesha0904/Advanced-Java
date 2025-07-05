3617e6bdb6eed21c17667c1780d5704fe50d8d41
Modi Sireesha <modisireesha09@gmail.com>
2025-07-05 20:22:39 +0530

    Remove the N-th Node from the End

--- AdvancedJava/src/DSA/LinkedLists/NthNodeRemove.java
+++ AdvancedJava/src/DSA/LinkedLists/NthNodeRemove.java
@@ -8,8 +8,36 @@ class NNode {
 		this.next = null;
 	}
 }
+
 public class NthNodeRemove {
 
+	static NNode endNodeRemove(NNode head, int n) {
+		NNode dummy = new NNode(0);
+		dummy = head;
+		
+		NNode first = dummy;
+		NNode second = dummy;
+		for(int i=0; i<=n; i++) {
+			first = first.next;
+		}
+		
+		while(first != null) {
+			first = first.next;
+			second = second.next;
+		}
+		second.next = second.next.next;
+		
+		NNode curr = head;
+		while(curr != null) {
+			System.out.println(curr.data + "->");
+			curr = curr.next;
+		}
+		System.out.println("null");
+		
+		
+		return dummy.next;
+	}
+	
 	public static void main(String[] args) {
 		
 	}
