1b16b8c02d959d4bd40713d249e6e01b6ed6fbab
Modi Sireesha <modisireesha09@gmail.com>
2025-07-08 20:12:40 +0530

    Rotate a Linked List

--- AdvancedJava/src/DSA/LinkedLists/RotateLinkedList.java
+++ AdvancedJava/src/DSA/LinkedLists/RotateLinkedList.java
@@ -10,13 +10,38 @@ class RoNode {
 }
 public class RotateLinkedList {
 
+	static RoNode rotateList(RoNode head, int k) {
+		
+		int length = 1;
+		RoNode tail = head;
+		
+		while(tail.next != null) {
+			tail = tail.next;
+			length++;
+		}
+		
+		k = k%length;
+		if(k==0) return head;
+		
+		int stepsToTail = (length - k);
+		RoNode newTail = head;
+		for(int i=1;i<stepsToTail; i++) {
+			newTail = newTail.next;
+		}
+		
+		RoNode newHead = newTail.next;
+		newTail.next = null;
+		tail.next = head;
+		
+		return newHead;
+	}
 	static void printList(RoNode head) {
 		RoNode curr = head;
 		while(curr != null) {
-			System.out.println(curr.data + "-> ");
+			System.out.print(curr.data + "-> ");
 			curr = curr.next;
 		}
-		System.out.println();
+		System.out.println("null");
 	}
 	public static void main(String[] args) {
 		
@@ -28,6 +53,11 @@ public class RotateLinkedList {
 		
 		System.out.println("Original List : ");
 		printList(list);
+		
+		int k = 2;
+		RoNode res  = rotateList(list,k);
+		System.out.println("Rotated List : ");
+		printList(res);
 		
 	}
 }
