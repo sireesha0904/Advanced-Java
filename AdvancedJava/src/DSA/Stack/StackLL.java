0a77f246a05d0a06b7596da053a27212d120d5cd
Modi Sireesha <modisireesha09@gmail.com>
2025-07-09 13:08:35 +0530

    Linked List-Based Stack Implementation

--- AdvancedJava/src/DSA/Stack/StackLL.java
+++ AdvancedJava/src/DSA/Stack/StackLL.java
@@ -11,7 +11,28 @@ class Node {
 }
 public class StackLL {
 
+	Node top = null;
+	
+	void push(int x) {
+		Node newNode = new Node(x);
+		newNode.next = top;
+		top = newNode;
+		System.out.println("Pushed : "+x);
+	}
+	
+	void display() {
+		Node temp = top;
+		while(temp != null) {
+			System.out.print(temp.data + "->");
+			temp = temp.next;
+		}
+		System.out.println("null");
+	}
 	public static void main(String[] args) {
+		StackLL s = new StackLL();
 		
+		s.push(10);
+		s.push(20);
+		s.display();
 	}
 }
