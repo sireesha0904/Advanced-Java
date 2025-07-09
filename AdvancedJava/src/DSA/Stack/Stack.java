0e391558a93f1b91f55ad6a0b0d50a11097abe2a
Modi Sireesha <modisireesha09@gmail.com>
2025-07-09 10:53:17 +0530

    Stack Implementation using array

---
+++ AdvancedJava/src/DSA/Stack/Stack.java
@@ -0,0 +1,18 @@
+package DSA.Stack;
+
+public class Stack {
+
+	int top;
+	int arr[];
+	
+	Stack(int size){
+		arr = new int[size];
+		top = -1;
+	}
+	
+	// push operation
+	
+	public static void main(String[] args) {
+		
+	}
+}
