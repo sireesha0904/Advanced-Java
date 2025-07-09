1890493f9dccea6202a722daf4397758b7306554
Modi Sireesha <modisireesha09@gmail.com>
2025-07-09 11:04:43 +0530

    Stack Implementation using array

--- AdvancedJava/src/DSA/Stack/Stack.java
+++ AdvancedJava/src/DSA/Stack/Stack.java
@@ -11,8 +11,50 @@ public class Stack {
 	}
 	
 	// push operation
+	void push(int x) {
+		if(top == arr.length - 1) {
+			System.out.println("Stack overflow, cannot add element" + x);
+		}
+		else {
+			top++;
+			arr[top] = x;
+			System.out.println("Pushed : "+x);
+		}
+	}
 	
+	//pop elements 
+	int pop() {
+		if(top == -1) {
+			System.out.println("Stack underflow, cannot remove element ");
+			return -1;
+		}
+		else {
+			int val = arr[top];
+			top --;
+			 System.out.println("Popped: " + val);
+			 return val;
+		}
+	
+	}
+	void display() {
+		if(top == -1) {
+			System.out.println("Stack is empty.");
+		} else {
+			 System.out.print("Stack elements: ");
+			 for(int i=0; i<=top;i++) {
+				 System.out.print(arr[i] + " ");
+			 }
+			 System.out.println();
+		}
+	}
 	public static void main(String[] args) {
+		Stack s = new Stack(5);
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		s.display();
 		
+		s.pop();
+		s.display();
 	}
 }
