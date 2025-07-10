d33a63ad06fa7d54f56b5c8d427caf8194269ca3
Modi Sireesha <modisireesha09@gmail.com>
2025-07-10 08:13:41 +0530

    ValidParentheses

--- AdvancedJava/src/DSA/Stack/ValidParentheses.java
+++ AdvancedJava/src/DSA/Stack/ValidParentheses.java
@@ -1,5 +1,31 @@
 package DSA.Stack;
-
+import java.util.Stack;
 public class ValidParentheses {
 
+	public static boolean isValid(String s) {
+		  Stack<Character> stack = new Stack<>();
+		  
+		  for(char ch : s.toCharArray()) {
+			  if(ch == '(' || ch == '[' || ch == '{') {
+				  stack.push(ch);
+			  }
+			  else {
+				  if(stack.isEmpty()) return false;
+			  }
+			  
+			  char top = stack.pop();
+			  if((top == ')' && top != '(') ||
+			  (top == '}' && top != '{') ||
+			  (top == ']' && top != ']')) {
+				  return false;
+			  }
+		  }
+		  return stack.isEmpty();
+	}
+	public static void main(String[] args) {
+		
+		System.out.println(isValid("(())"));
+		System.out.println(isValid("((])"));
+		System.out.println(isValid("{(())}[]"));
+	}
 }
