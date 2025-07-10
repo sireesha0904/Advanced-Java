b58ac9eb6e8a98af90446f225add4c82dc383317
Modi Sireesha <modisireesha09@gmail.com>
2025-07-10 21:00:12 +0530

    Reverse a String using Stack

--- AdvancedJava/src/DSA/Stack/ReverseString.java
+++ AdvancedJava/src/DSA/Stack/ReverseString.java
@@ -5,9 +5,21 @@ public class ReverseString {
 public static String reverse(String s) {
 	Stack<Character> stack = new Stack<>();
 	
+	for(char ch : s.toCharArray()) {
+		stack.push(ch);
+	}
+	
+	StringBuilder rev = new StringBuilder();
+	while(!stack.isEmpty()) {
+		rev.append(stack.pop());
+	}
+	
+	return rev.toString();
 }
 	public static void main(String[] args) {
-		
+		String s = "siri";
+		System.out.println("Original String : "+s);
+		System.out.println("Reverse String " + reverse(s));
 	}
 }
 
