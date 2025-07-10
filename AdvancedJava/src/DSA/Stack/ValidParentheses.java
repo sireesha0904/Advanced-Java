51d9e7b68bb2d8c0bd6594732d286d09f1482aff
Modi Sireesha <modisireesha09@gmail.com>
2025-07-10 20:31:30 +0530

    Valid Parentheses

--- AdvancedJava/src/DSA/Stack/ValidParentheses.java
+++ AdvancedJava/src/DSA/Stack/ValidParentheses.java
@@ -11,21 +11,24 @@ public class ValidParentheses {
 			  }
 			  else {
 				  if(stack.isEmpty()) return false;
+				  
+				  char top = stack.pop();
+				  
+				  if((ch == ')' && top != '(') ||
+				  (ch == '}' && top != '{') ||
+				  (ch == ']' && top != '[')) {
+					  return false;
+				  }
 			  }
-			  
-			  char top = stack.pop();
-			  if((top == ')' && top != '(') ||
-			  (top == '}' && top != '{') ||
-			  (top == ']' && top != ']')) {
-				  return false;
-			  }
+			 
 		  }
 		  return stack.isEmpty();
 	}
 	public static void main(String[] args) {
 		
-		System.out.println(isValid("(())"));
-		System.out.println(isValid("((])"));
-		System.out.println(isValid("{(())}[]"));
+		    System.out.println(isValid("()"));       // true
+	        System.out.println(isValid("({[]})"));   // true
+	        System.out.println(isValid("([)]"));     // false
+	        System.out.println(isValid("{["));       //false
 	}
 }
