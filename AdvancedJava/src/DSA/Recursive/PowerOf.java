cab49e1b2296ed83d4b9992c2d0570e0afaccb9e
Modi Sireesha <modisireesha09@gmail.com>
2025-07-22 13:00:06 +0530

    Basic Power (a^b)

---
+++ AdvancedJava/src/DSA/Recursive/PowerOf.java
@@ -0,0 +1,26 @@
+package DSA.Recursive;
+
+import java.util.Scanner;
+
+public class PowerOf {
+
+	int power(int a, int b) {
+		if(b==0) return 1;
+		return a * power(a,b-1);
+		
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a ");
+		int a = sc.nextInt();
+		
+		System.out.print("Enter b ");
+		int b = sc.nextInt();
+		PowerOf p = new PowerOf();
+		int res = p.power(a, b);
+		
+		System.out.println("Power of "+a+"^" + b + " = "+res	);
+		
+	}
+}
