4ef53b19dd746939f8e12720fa428225ef7f8e8e
Modi Sireesha <modisireesha09@gmail.com>
2025-07-22 12:34:40 +0530

    Recursive Fibonacci

--- AdvancedJava/src/DSA/Recursive/FibonacciSer.java
+++ AdvancedJava/src/DSA/Recursive/FibonacciSer.java
@@ -1,10 +1,22 @@
 package DSA.Recursive;
 
+import java.util.Scanner;
+
 public class FibonacciSer {
 
+	int fib(int n) {
+		if(n<=1) return n;
+		return fib(n-1) + fib(n-2);
+	}
 	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter N - ");
+	int n = sc.nextInt();
 	
-
+	FibonacciSer f = new FibonacciSer();
+	int res = f.fib(n);
+	System.out.print("Fibonacci of " + n + " is "+ res);
+	
 	}
 
 }
