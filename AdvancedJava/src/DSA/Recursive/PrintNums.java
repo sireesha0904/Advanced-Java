7f41d9876f659445c0ceb0130437232d749564e7
Modi Sireesha <modisireesha09@gmail.com>
2025-07-22 08:49:28 +0530

    Recursive basics print numbers 1 to N

--- AdvancedJava/src/DSA/Recursive/PrintNums.java
+++ AdvancedJava/src/DSA/Recursive/PrintNums.java
@@ -7,7 +7,7 @@ public class PrintNums {
 	void print1toN(int n) {
 		if(n ==0) return ;
 		print1toN(n - 1);
-		System.out.print(n + " ");
+		System.out.print(n + "");
 	}
 	public static void main(String[] args) {
 		
