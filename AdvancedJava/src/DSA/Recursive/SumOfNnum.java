329faf86cca09c1341c9b75c6242a85d790f4796
Modi Sireesha <modisireesha09@gmail.com>
2025-07-22 21:46:24 +0530

    Find the sum of first N numbers

--- AdvancedJava/src/DSA/Recursive/SumOfNnum.java
+++ AdvancedJava/src/DSA/Recursive/SumOfNnum.java
@@ -1,9 +1,18 @@
 package DSA.Recursive;
 
+import java.util.Scanner;
+
 public class SumOfNnum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter N - ");
+		int n = sc.nextInt();
+		
+		int sum = n * (n+1)/2 ;
+		System.out.println("Sum of "+n+" number is "+sum);
 
 	}
 
