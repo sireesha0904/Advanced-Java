7bcd6b46cde210b7cc121b96fcceb1a6d8b22ae8
Modi Sireesha <modisireesha09@gmail.com>
2025-07-22 08:49:22 +0530

    Recursive basics print numbers 1 to N

--- AdvancedJava/src/DSA/Recursive/PrintNums.java
+++ AdvancedJava/src/DSA/Recursive/PrintNums.java
@@ -1,9 +1,20 @@
 package DSA.Recursive;
 
+import java.util.Scanner;
+
 public class PrintNums {
 
+	void print1toN(int n) {
+		if(n ==0) return ;
+		print1toN(n - 1);
+		System.out.print(n + " ");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter N value - ");
+		int n = sc.nextInt();
+		
 
 	}
 
