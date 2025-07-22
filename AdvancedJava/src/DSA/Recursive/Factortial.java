36b79d9ca36623be44c199f9ceb6dc39bd0dcd18
Modi Sireesha <modisireesha09@gmail.com>
2025-07-22 12:27:03 +0530

    Factorial using recursion

--- AdvancedJava/src/DSA/Recursive/Factortial.java
+++ AdvancedJava/src/DSA/Recursive/Factortial.java
@@ -1,10 +1,24 @@
 package DSA.Recursive;
 
+import java.util.Scanner;
+
 public class Factortial {
 
+	int factorial(int n) {
+		if(n==0 || n==1) {
+			return n;
+		}
+		return n * factorial(n-1);
+	}
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter N - ");
+		int n = sc.nextInt();
 		
-
+		Factortial f = new Factortial();
+		int res = f.factorial(n);
+		System.out.print("Factorial of " + n + " is " + res);
+		
 	}
 
 }
