71acc10e21075efe9d1186134b3867405339f4d6
Modi Sireesha <modisireesha09@gmail.com>
2025-07-23 21:07:11 +0530

    Sum of Array using recursive

---
+++ AdvancedJava/src/DSA/Recursive/SumOfArray.java
@@ -0,0 +1,15 @@
+package DSA.Recursive;
+
+public class SumOfArray {
+
+	int sum(int[] arr, int i) {
+		if(i == arr.length) return 0;
+		return arr[i] + sum(arr, i+1);
+	}
+	public static void main(String[] args) {
+		SumOfArray s = new SumOfArray();
+		int[] arr = {1,2,3,4};
+		
+	}
+
+}
