fb3ff006f51e6d74f241398425eca01b5dae962f
Modi Sireesha <modisireesha09@gmail.com>
2025-07-23 21:42:53 +0530

    Palindrome Check (Recursive)

--- AdvancedJava/src/DSA/Recursive/PalindromeCheck.java
+++ AdvancedJava/src/DSA/Recursive/PalindromeCheck.java
@@ -9,7 +9,7 @@ public class PalindromeCheck {
 	}
 	public static void main(String[] args) {
 		PalindromeCheck pc = new PalindromeCheck();
-		String s = "aam";
+		String s = "madam";
 		boolean res = pc.isPalindrome(s, 0, s.length()-1);
 		  if(res) {
 			  System.out.println(s + " is a palindrome.");
