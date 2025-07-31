1464ede9bbed381f381c3ac1165ccace76051d8d
Modi Sireesha <modisireesha09@gmail.com>
2025-07-31 08:07:26 +0530

    Right-aligned triangle

--- AdvancedJava/src/DSA/Patterns/RightAngleTri.java
+++ AdvancedJava/src/DSA/Patterns/RightAngleTri.java
@@ -10,7 +10,7 @@ public class RightAngleTri {
 		int n = sc.nextInt();
 		
 		for(int i=0; i<=n; i++) {
-			for(int j=0; j<i;j++) {
+			for(int j=1; j<n-i;j++) {
 				System.out.print(" ");
 			}
 			for(int k=0; k<=i;k++) {
