ae4efc739f92365fbd59deaf4c19de79843d440f
Modi Sireesha <modisireesha09@gmail.com>
2025-07-31 09:00:24 +0530

    inverted diamond pattern

--- AdvancedJava/src/DSA/Patterns/DimondShape.java
+++ AdvancedJava/src/DSA/Patterns/DimondShape.java
@@ -1,8 +1,22 @@
 package DSA.Patterns;
 
+import java.util.Scanner;
+
 public class DimondShape {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n - ");
+		int n = sc.nextInt();
 		
+		for(int i = 0; i<n; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<n-i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 	}
 }
