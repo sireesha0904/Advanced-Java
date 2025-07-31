6dcbe3c0446d5e5f32c442a6c7f6ca092db81e16
Modi Sireesha <modisireesha09@gmail.com>
2025-07-31 08:13:45 +0530

    Pyramid

--- AdvancedJava/src/DSA/Patterns/TriangleStart.java
+++ AdvancedJava/src/DSA/Patterns/TriangleStart.java
@@ -10,11 +10,11 @@ public class TriangleStart {
 		int n= sc.nextInt();
 		
 		for(int i=1; i<=n; i++) {
-			for(int j = 1; j<n; j++) {
+			for(int j = i; j<n; j++) {
 				System.out.print(" ");
 			}
 			for(int k=1; k<=i; k++) {
-				System.out.print(" * ");
+				System.out.print("* ");
 			}
 			System.out.println();
 		}
