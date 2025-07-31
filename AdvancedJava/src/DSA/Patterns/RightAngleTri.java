31542f15029671dafede5f4c63b0d0a4755d639c
Modi Sireesha <modisireesha09@gmail.com>
2025-07-31 07:53:23 +0530

    Right-aligned triangle

--- AdvancedJava/src/DSA/Patterns/RightAngleTri.java
+++ AdvancedJava/src/DSA/Patterns/RightAngleTri.java
@@ -1,5 +1,23 @@
 package DSA.Patterns;
 
+import java.util.Scanner;
+
 public class RightAngleTri {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n value - ");
+		int n = sc.nextInt();
+		
+		for(int i=0; i<=n; i++) {
+			for(int j=0; j<i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<=i;k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
 }
