1f1af84fd46f7a84d01a9639b062542816dc4ecf
Modi Sireesha <modisireesha09@gmail.com>
2025-08-01 21:28:06 +0530

    Binary Tree Base Template

--- AdvancedJava/src/DSA/Tree/Main.java
+++ AdvancedJava/src/DSA/Tree/Main.java
@@ -1,10 +1,25 @@
 package DSA.Tree;
-
+class TreeNode {
+	int val;
+	TreeNode left, right;
+	
+	TreeNode(int val){
+		this.val = val;
+		left = right = null;
+	}
+}
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		
+		System.out.print("Root val : "+root.val);
 	}
 
 }
